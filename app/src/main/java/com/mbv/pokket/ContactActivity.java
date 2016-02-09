86ef65ae98c36b2fdaa3fb3d42c2f5a0673c34fe
strider2023 <strider2023@gmail.com>
2016-02-09 22:57:26 +0530

    mobile
    - Added contact details ui changes

--- app/src/main/java/com/mbv/pokket/ContactActivity.java
+++ app/src/main/java/com/mbv/pokket/ContactActivity.java
@@ -2,18 +2,47 @@ package com.mbv.pokket;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
 /**
  * Created by arindamnath on 04/02/16.
  */
 public class ContactActivity extends AppCompatActivity {
 
+    private Spinner querySpinner, loanSpinner;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        querySpinner = (Spinner) findViewById(R.id.contact_query_type_spinner);
+        loanSpinner = (Spinner) findViewById(R.id.contact_loan_id_spinner);
+
+        querySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(position == 1) {
+                    loanSpinner.setVisibility(View.VISIBLE);
+                    findViewById(R.id.contact_loan_id_lable).setVisibility(View.VISIBLE);
+                } else {
+                    loanSpinner.setVisibility(View.GONE);
+                    findViewById(R.id.contact_loan_id_lable).setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/mbv/pokket/UserProfileActivity.java
+++ app/src/main/java/com/mbv/pokket/UserProfileActivity.java
@@ -3,12 +3,29 @@ package com.mbv.pokket;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ListView;
 
-public class UserProfileActivity extends AppCompatActivity {
+import com.baoyz.widget.PullRefreshLayout;
+import com.mbv.pokket.adapters.TimelineListAdapter;
+import com.mbv.pokket.dao.TimelineDAO;
+import com.mbv.pokket.dao.UserProfileDAO;
+import com.mbv.pokket.dao.enums.LoaderID;
+import com.mbv.pokket.threads.TimelineLoaderTask;
+
+import java.util.ArrayList;
+
+public class UserProfileActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<UserProfileDAO> {
+
+    private TimelineListAdapter timelineListAdapter;
+    private PullRefreshLayout pullRefreshLayout;
+    private Bundle queryData;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +46,32 @@ public class UserProfileActivity extends AppCompatActivity {
 
             }
         });
+
+        timelineListAdapter = new TimelineListAdapter(this);
+        pullRefreshLayout = (PullRefreshLayout) findViewById(R.id.user_profile_detials_refresh_container);
+        listView = (ListView) findViewById(R.id.user_profile_detials_timeline_list);
+
+        pullRefreshLayout.setRefreshing(false);
+        listView.setAdapter(timelineListAdapter);
+
+        pullRefreshLayout.setOnRefreshListener(new PullRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                queryData = new Bundle();
+                queryData.putString("query", "");
+                getSupportLoaderManager()
+                        .initLoader(LoaderID.TIMELINE.getValue(), queryData, UserProfileActivity.this).forceLoad();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        queryData = new Bundle();
+        queryData.putString("query", "");
+        getSupportLoaderManager()
+                .initLoader(LoaderID.TIMELINE.getValue(), queryData, this).forceLoad();
     }
 
     @Override
@@ -40,5 +83,25 @@ public class UserProfileActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public Loader<UserProfileDAO> onCreateLoader(int id, Bundle args) {
+        return new TimelineLoaderTask(this, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<UserProfileDAO> loader, UserProfileDAO data) {
+        pullRefreshLayout.setRefreshing(false);
+        if(data != null) {
+            timelineListAdapter.setData(data.getTimelineDAOList());
+        } else {
+            timelineListAdapter.setData(new ArrayList<TimelineDAO>());
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<UserProfileDAO> loader) {
+        pullRefreshLayout.setRefreshing(false);
     }
 }
